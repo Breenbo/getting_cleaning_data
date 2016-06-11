b6989324d07d07e74133e6c57231cc285ae030ee
Breenbo <berrehuelbruno@gmail.com>
2016-06-11 15:23:34 +0200

    Modifications qui seront validées : 	nouveau fichier: script.R  Fichiers non suivis: 	.todo.txt.swp 	UCI_HAR_Dataset/ 	getdata%2Fprojectfiles%2FUCI_HAR_Dataset.zip 	todo.txt

---
+++ script.R
@@ -0,0 +1,8 @@
+## data frame -> working with dplyr
+library(dplyr)
+
+## memento of files
+## train/x_train : result per lines - 7352 obs / 561 variables
+## train/y_train : activities per lines, from 1 to 6 - 7352 obs / 1 column
+## train/subject_train : subject per lines, from 1 to 30 - 7352 obs / 1 column
+## activity_labels : as title, 6 obs / 2 columns (nb / name)
